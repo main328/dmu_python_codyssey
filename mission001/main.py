a17a26e8aa22aa8d26d2dbfd36b7574bf237cb1b
김현식 <kimhyunsik810@gmail.com>
2025-03-18 19:47:57 +0900

    002_수행과제001_코드 수정.

--- mission001/main.py
+++ mission001/main.py
@@ -1,6 +1,6 @@
 # log 파일 읽기.
 try:
-    with open('codyssey001/mission_computer_main.log', 'r', encoding='utf-8') as log_file:
+    with open('mission001/mission_computer_main.log', 'r', encoding='utf-8') as log_file:
         # 효율적인 메모리 사용을 위해 readlines() 사용.
         log_lines = log_file.readlines()
         for line in log_lines:
@@ -17,7 +17,7 @@ except Exception as error:
 
 # 사고 원인 보고서 작성.
 try:
-    with open('codyssey001/log_analysis.md', 'w', encoding='utf-8') as error_report:
+    with open('mission001/log_analysis.md', 'w', encoding='utf-8') as error_report:
         data = '성공적으로 도착했으나 산소 탱크가 폭발해 센터와 제어 시스템이 꺼졌다.'
         error_report.write(data)
         print('원인 보고서가 생성되었습니다.')
