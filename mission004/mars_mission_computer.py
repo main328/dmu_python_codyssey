4f88e7e32d6de51598d0c18c6d671973880cb8bb
김현식 <kimhyunsik810@gmail.com>
2025-04-03 20:00:26 +0900

    012_수행과제004_코드 수정.

--- mission004/mars_mission_computer.py
+++ mission004/mars_mission_computer.py
@@ -1,5 +1,4 @@
 import time
-import threading
 from dummy_sensor import DummySensor
 
 # 환경 변수명 정의
@@ -24,37 +23,34 @@ class MissionComputer:
         }
         # 5분 동안 환경 변수의 평균 값을 계산하기 위한 List
         self._data_log = []
-        # 종료 플래그.
-        self._stop_signal = False
 
     # 5초마다 환경 값을 갱신하고 5분마다 평균 값을 출력하는 함수.
     def get_sensor_data(self, sensor):
         start_time = time.time()
-        input_thread = threading.Thread(target=self._check_stop, daemon=True)
-        input_thread.start()
         
-        while not self._stop_signal:
-            sensor.set_env(self._env_values)
-            self._env_values = sensor.get_env()
+        try:
+            while not self._stop_signal:
+                sensor.set_env(self._env_values)
+                self._env_values = sensor.get_env()
 
-            print("{")
-            for key, value in self._env_values.items():
-                print(f'    "{key}": {value:.2f},')
-            print("}")
+                print("{")
+                for key, value in self._env_values.items():
+                    print(f'    "{key}": {value:.2f},')
+                print("}")
 
-            # 데이터 저장
-            self._data_log.append(self._env_values.copy())
+                # 데이터 저장
+                self._data_log.append(self._env_values.copy())
 
-            # 5분(300초) 경과 시 평균값 출력
-            if time.time() - start_time >= 300:
-                self.print_five_minute_average()
-                self._data_log.clear()
-                start_time = time.time()
-
-            # 5초 대기
-            time.sleep(5)
-
-        print('System stopped...')
+                # 5분(300초) 경과 시 평균값 출력
+                if time.time() - start_time >= 300:
+                    self.print_five_minute_average()
+                    self._data_log.clear()
+                    start_time = time.time()
+                # 5초 대기
+                time.sleep(5)
+        except KeyboardInterrupt:   
+            # 반복 중단 (Ctrl+C 입력 시 실행)
+            print('\nSystem stopped....')
 
     # 5분 동안 수집된 데이터를 환경 변수별로 평균 값을 계산하고 출력하는 함수.
     def print_five_minute_average(self):
@@ -76,12 +72,6 @@ class MissionComputer:
         for key, value in avg_values.items():
             print(f'    {key}: {value:.2f}')
         print("}")
-
-    # 사용자가 특정 값(Enter)을 입력하면 반복문을 종료하는 함수.
-    def _check_stop(self):
-        input("중지하려면 Enter 키를 입력하세요...\n")
-        self._stop_signal = True
-
 
 # 실행 코드
 if __name__ == '__main__':
